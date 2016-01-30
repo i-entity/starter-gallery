fa8fd5f3ce72d62381d72fbb5b806d8ebfee713a
George Deng <gndeng@live.com>
2016-01-29 16:17:15 -0800

    commit 3 part 3 completed

---
+++ application/views/_cell.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * The Original HTML
+ *   <a href="./data/Lands_End_Cape_Cornwall.jpg" data-lightbox="gallery"
+ *               data-title="Land's End, Cape Cornwall and the Brisons ... Posted 2014.05.04 by mickey, in landscape"><img
+ *                    src="./data/thumb/Lands_End_Cape_Cornwall.jpg"/></a>
+ */
+?>
+<a href="./data/{filename}" data-lightbox ="gallery"
+   data-title ="{title} ... Posted {uploaded} by {uploader} in {category}">
+    <img src='./data/thumb/{filename}'/>
+    
+</a>
